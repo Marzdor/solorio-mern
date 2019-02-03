4ddf17eab265784203ddf8f82f7ed4e95de66674
Max Ingersoll <marzdor@gmail.com>
2019-02-02 19:15:37 -0800

    removed old self made system add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,58 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
+// Components
 import Header from "./assests/components/Header";
 import Home from "./assests/components/Home";
+import Menu from "./assests/components/Menu";
+import About from "./assests/components/About";
+import Contact from "./assests/components/Contact";
+import Footer from "./assests/components/Footer";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contact: {
+        name: "",
+        email: "",
+        subscribe: false,
+        subject: "",
+        message: ""
+      }
+    };
+  }
   render() {
     return (
       <article className="container">
-        <Header handleNavClick={this.handleNavClick} />
-        <Home />
-        <Footer />
+        <Router>
+          <div>
+            <Header handleNavClick={this.handleNavClick} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/menu" component={Menu} />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/contact"
+                render={props => (
+                  <Contact
+                    {...props}
+                    contact={this.state.contact}
+                    handleChange={this.handleChange}
+                    handleCheckClick={this.handleChange}
+                  />
+                )}
+              />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
       </article>
     );
   }
--- client/src/assests/components/Header.js
+++ client/src/assests/components/Header.js
@@ -1,36 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = props => {
   return (
     <header className="header-container">
-      <a
-        id="logo-big"
-        className="logo"
-        onClick={props.handleNavClick}
-        href="#Home"
-      >
+      <Link id="logo-big" className="logo" to="/">
         <img alt="Solorio Brewing CO. Logo" />
-      </a>
+      </Link>
       <nav className="nav">
         <div id="logo-small" className="logo-container">
-          <a className="logo" onClick={props.handleNavClick} href="#Home">
+          <Link className="logo" to="/">
             <img alt="Solorio Brewing CO. Logo" />
-          </a>
+          </Link>
           <div className="logo-container-sub">
             <h1 className="logo-title">Solorio Brewing Co.</h1>
             <h3 className="logo-title-sub">EST. 2015</h3>
             <h2 className="logo-title">Rancho Cucamonga</h2>
           </div>
         </div>
-        <a className="nav-link" onClick={props.handleNavClick} href="#Menu">
+        <Link className="nav-link" to="Menu">
           Menu
-        </a>
-        <a className="nav-link" onClick={props.handleNavClick} href="#About">
+        </Link>
+        <Link className="nav-link" to="About">
           About
-        </a>
-        <a className="nav-link" onClick={props.handleNavClick} href="#Contact">
+        </Link>
+        <Link className="nav-link" to="Contact">
           Contact
-        </a>
+        </Link>
       </nav>
     </header>
   );
