45cccbdd2fd7a174a8594a6945181bd6430698d3
Max Ingersoll <marzdor@gmail.com>
2019-02-03 20:19:09 -0800

    got postcss setup added css for edit page

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = () => ({
+  plugins: {
+    autoprefixer: {
+      browsers: "> 5%"
+    },
+    "postcss-import": {},
+    "postcss-mixins": {},
+    "postcss-nested": {},
+    "postcss-simple-vars": {}
+  }
+});
