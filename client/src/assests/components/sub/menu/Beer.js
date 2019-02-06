20f79a32f419abc530e6b77e95191907b7492e0a
Max Ingersoll <marzdor@gmail.com>
2019-02-05 21:27:02 -0800

    Added description to menu and changed layout a little

--- client/src/assests/components/sub/menu/Beer.js
+++ client/src/assests/components/sub/menu/Beer.js
@@ -31,11 +31,13 @@ const Beer = props => {
           <h3 className="menu-title-sub">ABV</h3>
           <p className="menu-text">{props.beer[key].abv}%</p>
 
-          <img
-            className="menu-image"
-            src="https://via.placeholder.com/175x150"
-            alt={"Image of " + props.beer[key].name + " beer."}
-          />
+          <div className="menu-image">
+            <img
+              src="https://via.placeholder.com/175x150"
+              alt={"Image of " + props.beer[key].name + " beer."}
+            />
+            <p className="menu-text">{props.beer[key].desc}</p>
+          </div>
         </div>
       );
     }
