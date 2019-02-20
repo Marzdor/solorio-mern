f3ac71a4f01a918f82c62f9760483059756bf6bc
Max Ingersoll <marzdor@gmail.com>
2019-02-19 20:05:30 -0800

    pulling info from db on home page

--- client/src/assests/components/Home.js
+++ client/src/assests/components/Home.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = props => {
-  return (
-    <section>
-      <iframe
-        id="video"
-        title="Company Video"
-        src="https://www.youtube.com/embed/bgeYi_JDO5k?rel=0&amp;showinfo=0"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-      />
-    </section>
-  );
-};
+import Days from "./sub/home/Days";
+import Events from "./sub/home/Events";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { days: {}, events: {} };
+  }
+  componentDidMount() {
+    fetch("/main/info/days")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ days: data });
+      });
+    fetch("/main/info/events")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ events: data });
+      });
+  }
+  render() {
+    return (
+      <article>
+        <Days days={this.state.days} />
+        <Events events={this.state.events} />
+      </article>
+    );
+  }
+}
 
 export default Home;
