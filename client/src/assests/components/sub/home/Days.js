06446c066c48d9d803a1c316147b4d1a32ef8cb4
Max Ingersoll <marzdor@gmail.com>
2019-02-20 18:26:01 -0800

    changed element gen based on if they are open

--- client/src/assests/components/sub/home/Days.js
+++ client/src/assests/components/sub/home/Days.js
@@ -4,19 +4,31 @@ const Days = props => {
   const dayEle = [];
   // loop through days of week to make elements
   for (let day in props.days) {
-    dayEle.push(
-      <section className="day" key={day}>
-        <h2 className="day-name">{day}</h2>
-        <h4 className="day-title">Hours</h4>
-        <p className="day-text">{props.days[day].hours}</p>
-        <h4 className="day-title">Happy Hour</h4>
-        <p className="day-text">{props.days[day].happyHour}</p>
-        <h4 className="day-title">Special</h4>
-        <p className="day-text">{props.days[day].special}</p>
-      </section>
-    );
+    let curEle;
+    // If closed reduce info being displayed for element
+    if (props.days[day].hours === "Closed") {
+      curEle = (
+        <section className="day day--closed" key={day}>
+          <h2 className="day-name">{day}</h2>
+          <h3 className="day-title">Closed</h3>
+        </section>
+      );
+    } else {
+      curEle = (
+        <section className="day" key={day}>
+          <h2 className="day-name">{day}</h2>
+          <h4 className="day-title">Hours</h4>
+          <p className="day-text">{props.days[day].hours}</p>
+          <h4 className="day-title">Happy Hour</h4>
+          <p className="day-text">{props.days[day].happyHour}</p>
+          <h4 className="day-title">Special</h4>
+          <p className="day-text">{props.days[day].special}</p>
+        </section>
+      );
+    }
+    dayEle.push(curEle);
   }
-  return <div className="week">{dayEle}</div>;
+  return <div className="home home-week">{dayEle}</div>;
 };
 
 export default Days;
